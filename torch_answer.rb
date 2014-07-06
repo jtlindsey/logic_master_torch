5f78929bba233e7a3016bf7ced0f156899ea856b
J Travis Lindsey <reachjtl@gmail.com>
2014-07-06 18:41:00 -0400

    first commit torch in progress

---
+++ torch_answer.rb
@@ -0,0 +1,104 @@
+class Torch
+	attr_accessor :time_left, :people, :torch, :right_group
+
+	def initialize(time_left=30)
+		@time_left = time_left
+		@people = [1,3,6,8,12]#[12,8,6,3,1] #[6,1,3,12,8]
+		@torch = 0
+		@right_group = []
+		@stop = false
+		@attemps = 0
+	end
+
+	def random_index_pos(people)
+		x = Random.new
+		random = 0
+		until people.include?(random)
+			random = x.rand(13)
+		end
+		random
+	end
+
+	def random_go_back(right_group)
+		x = Random.new
+		random = 0
+		until right_group.include?(random)
+			random = x.rand(13)
+		end
+		random
+	end
+
+	def pairs
+		while @stop == false
+
+			# @attemps += 1
+			# if @attemps >= 10
+			# 	@stop = true
+			# end
+
+			puts "enter loop peoples total #{@people.count}, who #{@people}"#
+			uz1 = Torch.new
+			runner_a = uz1.random_index_pos(@people)
+			@right_group << runner_a #add to right group totals
+			@people.delete(runner_a)
+			puts "1st people array edit new total #{@people.count}, who #{@people}"#remove...............
+
+			uz2 = Torch.new
+			runner_b = uz2.random_index_pos(@people)
+			@right_group << runner_b #add to right group totals
+			@people.delete(runner_b)
+			puts "2nd people array edit new total #{@people.count}, who #{@people}"#remove...............
+
+			#cross bridge and adjust torch and time
+			@torch = 1 #move torch to right
+			@time_left = @time_left - (runner_a + runner_b)
+
+			#check for cracked code before going back
+			puts "*******Totals, time left #{@time_left} right count #{@right_group.count}"
+
+			if @time_left >= 0 && @right_group.count == 5
+				puts "Code Cracked!"
+				puts "Attemps before success = #{@attemps}"
+				puts "People array #{@people.count}"
+				puts "People in right group #{@right_group.count}, time left #{@time_left}"
+				@stop = true
+			end
+
+			if @stop == false
+
+				puts "******runner A #{runner_a}, B #{runner_b}, time left #{@time_left}*****"
+				puts "before edit right group count #{@right_group.count}"#remove...............
+				puts "time left #{@time_left}"#remove...............
+
+				uz3 = Torch.new
+				runner_goback = uz3.random_go_back(@right_group)#1 of 2 runners on right go back to left
+				@right_group.delete(runner_goback) #remove from right group totals
+				@people << runner_goback #runner go back to group
+				torch = 0 #move torch to left
+				@time_left = @time_left - runner_goback #take runner time off clock
+				puts "*******gobacker******** #{runner_goback}"
+
+			if @time_left < 0
+				puts "************************game is resetting*****************"	
+				@time_left = 30 #reset clock
+				@people = [1,3,6,8,12] #reset people
+				@torch = 0 #reset torch position
+				@right_group = []# right group equal zero					
+			end
+
+			end
+		end
+	end
+end
+
+
+	puts "Hello, press x to start, or any other key to exit"
+	response = gets.chomp
+
+	case response
+	when "x"
+		query = Torch.new
+		query.pairs
+	else
+		puts "Goodbye"
+	end
